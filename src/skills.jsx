c4e8993bdc34664b5d054a4de7760d4bf63e73de
Arun Kumar <arunkumar413@gmail.com>
2022-10-10 10:17:07 +0000

    save

--- src/skills.jsx
+++ src/skills.jsx
@@ -4,7 +4,16 @@ export function Skills() {
   return (
     <div className="skills-container">
       <h2 className="project-heading">Skills:</h2>
-      <span className="skill">HTML</span>
+      <div className="skills-grid">
+        <div className="skill">HTML</div>
+        <div className="skill">CSS</div>
+        <div className="skill">JavsScript</div>
+        <div className="skill">NodeJs</div>
+        <div className="skill">ExpressJs</div>
+        <div className="skill">ReactJs</div>
+        <div className="skill">Material UI</div>
+        <div className="skill">ReactJs</div>
+      </div>
     </div>
   );
 }
