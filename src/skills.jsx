bc5fac5277e08ae0f6e639a49f2d298c6821601b
Arun Kumar <arunkumar413@gmail.com>
2022-10-10 10:06:26 +0000

    save

--- src/skills.jsx
+++ src/skills.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 export function Skills() {
-  return;
-
-  <div className="skills-container">
-    <h2 className="project-heading">Skills:</h2>
-    <span className="skill">HTML</span>
-  </div>;
+  return (
+    <div className="skills-container">
+      <h2 className="project-heading">Skills:</h2>
+      <span className="skill">HTML</span>
+    </div>
+  );
 }
